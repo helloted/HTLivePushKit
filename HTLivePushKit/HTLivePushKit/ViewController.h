9f6f2dc72ef05037b7341cf3a823bcadb39ed2db
iMac <helloted@qq.com>
2018-05-21 10:29:02 +0800

    init

---
+++ HTLivePushKit/HTLivePushKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTLivePushKit
+//
+//  Created by iMac on 2018/5/21.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HTLivePushKit/HTLivePushKit/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  HTLivePushKit
+//
+//  Created by iMac on 2018/5/21.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *switchBtn;
+@property (weak, nonatomic) IBOutlet UIButton *liveBtn;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
